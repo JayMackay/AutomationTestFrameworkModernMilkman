54c41522485518100f1a2257c6ffef11bfa37496
JayMackay <jared.mackay1994@live.com>
2021-03-25 19:10:47 +0000

    Updated Home Page Object Model With Constructor

--- AutomationTestFramework/Library/PageObjectModels/HomePage.cs
+++ AutomationTestFramework/Library/PageObjectModels/HomePage.cs
@@ -5,11 +5,24 @@ namespace AutomationTestFramework.Library.PageObjectModels
 {
     public class HomePage
     {
-        private IWebDriver driver;
+        private IWebDriver _driver;
         private string mainPageUrl = AppConfigReader.BaseUrl;
+        
+        //Page Elements
+        private IWebElement PostcodeInput => this._driver.FindElement(By.Id("postcode"));
+        private IWebElement FindButton => this._driver.FindElement(By.Id("checkPostcode5"));
+        private IWebElement SignInButton => this._driver.FindElement(By.XPath("/img/images/Sign In Button.svg"));
 
-        private IWebElement PostcodeInput => this.driver.FindElement(By.Id("postcode"));
-        private IWebElement FindButton => this.driver.FindElement(By.Id("checkPostcode5"));
-        private IWebElement SignInButton => this.driver.FindElement(By.XPath("/img/images/Sign In Button.svg"));
+        //Page Object Model Constructor
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        //Methods
+        public void VisitHomePage()
+        {
+            _driver.Navigate().GoToUrl(mainPageUrl);
+        }
     }
 }
