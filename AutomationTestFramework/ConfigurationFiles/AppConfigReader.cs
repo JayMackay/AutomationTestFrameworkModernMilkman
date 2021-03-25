ee9b6c6606ade92ffbafb6c8ed99d84595c28dca
JayMackay <jared.mackay1994@live.com>
2021-03-25 18:18:33 +0000

    Added Application Config & Reader

---
+++ AutomationTestFramework/ConfigurationFiles/AppConfigReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace AutomationTestFramework.ConfigurationFiles
+{
+    //Global reader for the App.config attributes
+    public static class AppConfigReader
+    {
+        public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+    }
+}
