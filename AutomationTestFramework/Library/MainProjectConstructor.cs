1bf5404248e868f37777ac2f4698d19438571ca9
JayMackay <jared.mackay1994@live.com>
2021-03-25 18:38:05 +0000

    Created Main Page Object Model Constructor

---
+++ AutomationTestFramework/Library/MainProjectConstructor.cs
@@ -0,0 +1,23 @@
+﻿using AutomationTestFramework.ConfigurationFiles;
+using AutomationTestFramework.Library.PageObjectModels;
+using OpenQA.Selenium;
+
+namespace AutomatedTestFramework.Library
+{
+    public class MainProjectConstructor
+    {
+        //Accessible read only page object models
+        public readonly HomePage WebsiteHomePage;
+        public readonly IWebDriver seleniumDriver;
+
+        //Constructor for driver and configurations for the service
+        public MainProjectConstructor(string driverName, int pageLoadWaitInSecs = 10, int implicitWaitTimeInSecs = 10)
+        {
+            //Instatation of driver
+            seleniumDriver = new SeleniumDriverConfig(driverName, pageLoadWaitInSecs, implicitWaitTimeInSecs).Driver;
+
+            //Instantation of page objects with the selenium driver
+            WebsiteHomePage = new HomePage(seleniumDriver);
+        }
+    }
+}
