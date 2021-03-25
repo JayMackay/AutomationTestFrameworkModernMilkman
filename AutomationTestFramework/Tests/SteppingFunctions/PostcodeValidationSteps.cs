0ccb7e17d3764eb421bf82637d07e8a6a4c4a7b3
JayMackay <jared.mackay1994@live.com>
2021-03-25 21:24:21 +0000

    Initialized Postcode Validation Stepping Functions

---
+++ AutomationTestFramework/Tests/SteppingFunctions/PostcodeValidationSteps.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using AutomatedTestFramework.Library;
+using TechTalk.SpecFlow;
+
+namespace AutomationTestFramework.Tests.SteppingFunctions
+{
+    [Binding]
+    public class PostcodeValidationSteps
+    {
+        private MainProjectConstructor _constructor;
+
+        [Given(@"I am on the website main home page")]
+        public void WebsiteMainHomePage()
+        {
+            _constructor = new MainProjectConstructor("Chrome");
+            _constructor.WebsiteHomePage.VisitHomePage();
+        }
+
+        [When(@"I input a valid postcode")]
+        public void InputValidPostcode()
+        {
+            _constructor.WebsiteHomePage.EnterAvailableValidPostcode("LS20 8JN");
+        }
+
+        [Then(@"I should be redirected to the registration page")]
+        public void RedirectToRegistrationPage()
+        {
+            Assert.That(_constructor.RegistrationPage.
+
+        }
+    }
+}
