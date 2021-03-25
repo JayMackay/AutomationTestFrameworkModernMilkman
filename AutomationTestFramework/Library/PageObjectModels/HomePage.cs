4f42d6b5ac300fa7cbee52407de348944e42b2ed
JayMackay <jared.mackay1994@live.com>
2021-03-25 18:30:22 +0000

    Created Website Home Page Object Model

---
+++ AutomationTestFramework/Library/PageObjectModels/HomePage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using AutomationTestFramework.ConfigurationFiles;
+
+namespace AutomationTestFramework.Library.PageObjectModels
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+        private string mainPageUrl = AppConfigReader.BaseUrl;
+
+        private IWebElement PostcodeInput => this.driver.FindElement(By.Id("postcode"));
+        private IWebElement FindButton => this.driver.FindElement(By.Id("checkPostcode5"));
+        private IWebElement SignInButton => this.driver.FindElement(By.XPath("/img/images/Sign In Button.svg"));
+    }
+}
