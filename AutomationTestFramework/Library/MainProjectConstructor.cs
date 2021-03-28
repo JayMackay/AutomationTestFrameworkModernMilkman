97cfeed6aa6c2018939529c26fc7280315a157d5
JayMackay <jared.mackay1994@live.com>
2021-03-28 20:34:10 +0100

    Added Additional Page Object Models For Account Update Tests & Updated Constructor

--- AutomationTestFramework/Library/MainProjectConstructor.cs
+++ AutomationTestFramework/Library/MainProjectConstructor.cs
@@ -6,26 +6,30 @@ namespace AutomationTestFramework.Library
 {
     public class MainProjectConstructor
     {
-        //Accessible read only page object models
+        //Accessible Read Only Page Object Models
         public readonly HomePage WebsiteHomePage;
         public readonly RegistrationPage WebsiteRegistrationPage;
         public readonly ServiceUnavailablePage WebsiteServiceUnavailablePage;
         public readonly UserHomePage WebsiteUserHomePage;
         public readonly LoginPage WebsiteLoginPage;
+        public readonly AccountPage WebsiteAccountPage;
+        public readonly UpdateAccountPage WebsiteUpdateAccountPage;
         public readonly IWebDriver seleniumDriver;
 
-        //Constructor for driver and configurations for the service
+        //Constructor For Driver & Configurations For The Service
         public MainProjectConstructor(string driverName, int pageLoadWaitInSecs = 10, int implicitWaitTimeInSecs = 10)
         {
-            //Instatation of driver
+            //Instantiation Of Driver
             seleniumDriver = new SeleniumDriverConfig(driverName, pageLoadWaitInSecs, implicitWaitTimeInSecs).Driver;
 
-            //Instantation of page objects with the selenium driver
+            //Instantiation Of Page Objects With The Selenium Driver
             WebsiteHomePage = new HomePage(seleniumDriver);
             WebsiteRegistrationPage = new RegistrationPage(seleniumDriver);
             WebsiteServiceUnavailablePage = new ServiceUnavailablePage(seleniumDriver);
             WebsiteUserHomePage = new UserHomePage(seleniumDriver);
             WebsiteLoginPage = new LoginPage(seleniumDriver);
+            WebsiteAccountPage = new AccountPage(seleniumDriver);
+            WebsiteUpdateAccountPage = new UpdateAccountPage(seleniumDriver);
         }
     }
 }
---
+++ AutomationTestFramework/Library/PageObjectModels/AccountPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using AutomationTestFramework.ConfigurationFiles;
+
+namespace AutomationTestFramework.Library.PageObjectModels
+{
+    public class AccountPage
+    {
+        private IWebDriver _driver;
+        private string mainPageUrl = AppConfigReader.BaseUrl;
+
+        //Page Elements
+        private IWebElement AccountDetailHeader => this._driver.FindElement(By.XPath("/html/body/section[1]/div/div/div[1]/div/h2[1]"));
+        private IWebElement EditAccountButton => this._driver.FindElement(By.XPath("/html/body/section[1]/div/div/div[2]/div/div[2]/a[1]/img"));
+
+        //Page Object Model Constructor
+        public AccountPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        //Methods
+        public string AccountDetailHeaderText()
+        {
+            return AccountDetailHeader.Text;
+        }
+
+        public void EditAccount()
+        {
+            EditAccountButton.Click();
+        }
+    }
+}
