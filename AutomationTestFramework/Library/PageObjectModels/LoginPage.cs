8022736bc440df9a0f12d1942ff17e2f8942259e
JayMackay <jared.mackay1994@live.com>
2021-03-28 17:13:43 +0100

    Added User Home & Login Page Object Models

---
+++ AutomationTestFramework/Library/PageObjectModels/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using AutomationTestFramework.ConfigurationFiles;
+
+namespace AutomationTestFramework.Library.PageObjectModels
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+        private string mainPageUrl = AppConfigReader.BaseUrl;
+
+        //Page Elements
+        private IWebElement LoginHeaderString => this._driver.FindElement(By.XPath("/html/body/section[@class='login-page']/div[@class='container']/div[@class='row']/div[@class='col-md-12']/div[@class='login-form']/h1"));
+        private IWebElement MobileNumberInput => this._driver.FindElement(By.Id("phoneNo"));
+        private IWebElement PasswordInput => this._driver.FindElement(By.Id("password"));
+        private IWebElement LoginButton => this._driver.FindElement(By.Id("btn_submit"));
+
+        //Page Object Model Constructor
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        //Methods
+        public string LoginHeaderCheck()
+        {
+            return LoginHeaderString.Text;
+        }
+
+        public void UserLogin(string username, string password)
+        {
+            MobileNumberInput.SendKeys(username);
+            PasswordInput.SendKeys(password);
+            LoginButton.Click();
+        }
+    }
+}
---
+++ AutomationTestFramework/Library/PageObjectModels/UserHomePage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using AutomationTestFramework.ConfigurationFiles;
+
+namespace AutomationTestFramework.Library.PageObjectModels
+{
+    public class UserHomePage
+    {
+        private IWebDriver _driver;
+        private string mainPageUrl = AppConfigReader.BaseUrl;
+
+        //Page Elements
+        private IWebElement MilkDelivery => this._driver.FindElement(By.XPath("/html/body[@class='login']/section[@class='categories-section']/div[@class='container']/div[@class='row']/div[@class='col-sm-12']/div[@class='row']/div[@class='col-6 col-sm-3'][1]/div[@class='categries-singleblocks']/div[@class='single-categories-name']/p/a"));
+
+        //Page Object Model Constructor
+        public UserHomePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        //Methods
+        public string UserMilkDeliveryCheck()
+        {
+            return MilkDelivery.Text;
+        }
+
+    }
+}
